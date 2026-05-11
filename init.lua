f32ccacb06121c1b8df90b94a807640c825d189c
Simon Pereira <simon.pereira@worklife.io>
2026-05-11 18:35:22 +0200

    remove love2d

--- init.lua
+++ init.lua
@@ -403,16 +403,6 @@ require('lazy').setup({
   },
   { 'vuciv/golf' },
   {
-    'S1M0N38/love2d.nvim',
-    event = 'VeryLazy',
-    opts = {},
-    keys = {
-      { '<leader>v', ft = 'lua', desc = 'LÖVE' },
-      { '<leader>vv', '<cmd>LoveRun<cr>', ft = 'lua', desc = 'Run LÖVE' },
-      { '<leader>vs', '<cmd>LoveStop<cr>', ft = 'lua', desc = 'Stop LÖVE' },
-    },
-  },
-  {
     'nvimtools/none-ls.nvim',
     dependencies = {
       'nvimtools/none-ls-extras.nvim',
